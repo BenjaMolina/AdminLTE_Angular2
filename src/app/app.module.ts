3a40296a62b8ecf3aa0e5f1e233b9de256a4a66b
Benjamin <molinasb.94@gmail.com>
2018-03-01 16:35:25 -0600

    Componentes principales agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingsComponent } from './components/appsettings/appsettings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppfooterComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppsettingsComponent
   ],
   imports: [
     BrowserModule,
